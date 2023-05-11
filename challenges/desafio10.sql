09bbb8e1569630eadfe21eb46d09b221cc9175ff
hildelio <hildelio@gmail.com>
2023-05-11 20:03:47 -0300

    Req 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,32 @@
--- Descomente e altere as linhas abaixo:
-
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+CREATE TABLE IF NOT EXISTS favorite_songs (
+  `users_id` INT NOT NULL,
+  `songs_id` INT NOT NULL,
+  PRIMARY KEY (`users_id`, `songs_id`),
+  INDEX `fk_users_has_songs_songs2_idx` (`songs_id` ASC) VISIBLE,
+  INDEX `fk_users_has_songs_users2_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_has_songs_users2`
+    FOREIGN KEY (`users_id`)
+    REFERENCES users (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_songs_songs2`
+    FOREIGN KEY (`songs_id`)
+    REFERENCES songs (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+INSERT INTO favorite_songs(`users_id`, `songs_id`)
+VALUES
+	('1', '3'),
+	('1', '6'),
+	('1', '10'),
+	('2', '4'),
+	('3', '1'),
+	('3', '3'),
+	('4', '7'),
+	('4', '4'),
+	('5', '10'),
+	('5', '2'),
+	('8', '4'),
+	('9', '7'),
+	('10', '3');
